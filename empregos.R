a7167cb411fd094f5367dbe69c7298e01f2bc408
Paulo Serra Filho <pauloserrafh@gmail.com>
2017-11-29 23:25:15 -0300

    Ordenando dataset por mesorregioes do estado de PE

--- empregos.R
+++ empregos.R
@@ -1,5 +1,6 @@
 # set working directory
-setwd(".\\datasets\\empregos-pe")
+#setwd(".\\datasets\\empregos-pe") #Para windows
+setwd("./datasets/empregos-pe") #Para linux
 # data
 data <- read.table("dataset.csv", header = TRUE, sep=";", encoding = "UTF-8")
 
@@ -11,3 +12,19 @@ mean(data$Serviços.industriais.de.utilidade.pública)
 mean(data$Comércio)
 mean(data$Serviços)
 mean(data$Administração.pública)
+
+#Os dados foram organizados baseado nas Mesorregiões do estado
+#Mesorregião do São Francisco Pernambucano
+#comeca em Petrolandia (linha 2) vai até Itacuruba(linha 16)
+
+#Mesorregião do Sertão Pernambucano
+#comeca em Araripina (linha 17) vai até Sertânia (linha 57)
+
+#Mesorregião do Agreste Pernambucano
+#comeca em Agrestina (linha 58) vai até Vertentes (linha 128)
+
+#Mesorregião da Zona da Mata Pernambucana
+#comeca em Chã de Alegria (linha 129) vai até Xexéu (linha 171)
+
+#Mesorregião Metropolitana do Recife
+#comeca em Abreu e Lima (linha 172) vai até São Lourenço da Mata (linha 186)
